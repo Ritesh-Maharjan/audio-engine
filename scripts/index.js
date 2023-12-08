d368c56ffccb3b7a2362e9bd2985a95befe53be9
Ritesh <riteshmaharjan07@gmail.com>
2023-12-07 20:22:38 -0800

    fixed the bug of chrome slider 2nd image overriding first image and added comments for better understanding

--- scripts/index.js
+++ scripts/index.js
@@ -9,36 +9,54 @@ const bannerRight = document.getElementById("bannerRight");
 console.log(activeClass);
 
 Array.from(spanCircle).forEach((el) => {
+  // adding event listener to all the dots
   el.addEventListener("click", (e) => {
+    // storing the clicked element
     const targetedCircle = e.target;
+    // taking the number 1 in order to use translate and go to that image place
     const targetedNum = e.target.className.split("-")[1][0];
+    // taking the number and adding transfrom to go to the targeted image
     imgContainer.style.transform = `translateX(-${100 * targetedNum}%)`;
+    // adding active status to the circle
     targetedCircle.classList.add("active");
+    // removing active class from old circle
     activeClass.classList.remove("active");
+    //   assinging active elements to the new element that is active
     activeClass = targetedCircle;
   });
 });
 
 bannerLeft.addEventListener("click", (e) => {
+  // targeting the active class and substracting 1 to go to previous picture
   let targetedNum = activeClass.className.split("-")[1][0] - 1;
 
+  //   if first screen, need to go to the last one
   if (targetedNum < 0) {
     targetedNum = 6;
   }
+  //   using transform function to go to the  image
   imgContainer.style.transform = `translateX(-${100 * targetedNum}%)`;
+  // adding active status to the circle
   spanCircle[targetedNum].classList.add("active");
+  // removing active class from old circle
   activeClass.classList.remove("active");
+  //   assinging active elements to the new element that is active
   activeClass = spanCircle[targetedNum];
 });
 
 bannerRight.addEventListener("click", (e) => {
-    let targetedNum = Number(activeClass.className.split("-")[1][0]) + 1;
-    if (targetedNum > 6) {
-      targetedNum = 0;
-    }
-    imgContainer.style.transform = `translateX(-${100 * targetedNum}%)`;
-    spanCircle[targetedNum].classList.add("active");
-    activeClass.classList.remove("active");
-    activeClass = spanCircle[targetedNum];
-  });
-  
+  // targeting the active class and substracting 1 to go to previous picture
+  let targetedNum = Number(activeClass.className.split("-")[1][0]) + 1;
+  //   if last screen, need to go to the first one
+  if (targetedNum > 6) {
+    targetedNum = 0;
+  }
+  //   using transform function to go to the  image
+  imgContainer.style.transform = `translateX(-${100 * targetedNum}%)`;
+  // adding active status to the circle
+  spanCircle[targetedNum].classList.add("active");
+  // removing active class from old circle
+  activeClass.classList.remove("active");
+  //   assinging active elements to the new element that is active
+  activeClass = spanCircle[targetedNum];
+});
